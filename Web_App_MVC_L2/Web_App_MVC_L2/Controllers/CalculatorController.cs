6328de861f4384099410094829672b10395857d2
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-10-21 20:30:30 +0200

    Calculator full v1

---
+++ Web_App_MVC_L2/Web_App_MVC_L2/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_App_MVC_L2.Models;
+
+namespace Web_App_MVC_L2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new CalculatorModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model)
+        {
+            model.Calculate();
+            return View(model);
+        }
+    }
+}
---
+++ Web_App_MVC_L2/Web_App_MVC_L2/Models/CalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Web_App_MVC_L2.Models
+{
+    public class CalculatorModel
+    {
+        public double Number1 { get; set; }
+        public double Number2 { get; set; }
+        public double? Result { get; set; }
+        public string? Operation { get; set; }
+
+        public void Calculate()
+        {
+            switch (Operation)
+            {
+                case "Add":
+                    Result = Number1 + Number2;
+                    break;
+                case "Substract":
+                    Result = Number1 - Number2;
+                    break;
+                case "Multiply":
+                    Result = Number1 * Number2;
+                    break;
+                case "Devide":
+                    Result = Number2 != 0 ? Number1 / Number2 : double.NaN;
+                    break;
+                default:
+                    Result = 0;
+                    break;
+            }
+        }
+    }
+}
